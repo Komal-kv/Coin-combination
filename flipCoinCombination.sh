2996d59774c6748ffbc0bb2ae7b668a5fa4fb663
Komal <divyavairagade@gmail.com>
2022-04-03 15:26:35 +0530

    triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,59 +1,87 @@
 #!/bin/bash
 
-HH=0;
-HT=0;
-TH=0;
-TT=0;
+HHH=0;
+HHT=0;
+HTH=0;
+HTT=0;
+THH=0;
+THT=0;
+TTH=0;
+TTT=0;
 
-while [[ $HH -ne 5 && $HT -ne 5 && $TH -ne 5 && $TT -ne 5 ]]
+while [[ $HHH -ne 5 && $HHT -ne 5 && $HTH -ne 5 && $HTT -ne 5 ]]
 do
 	coin1=$((RANDOM%2));
+	a=$coin1;
 	coin2=$((RANDOM%2));
-	if [[ $coin1 -eq 0 && $coin2 -eq 0 ]]
+	b=$coin2;
+	coin3=$((RANDOM%2));
+	c=$coin3;
+	if [[ $a -eq 0 && $b -eq 0 && $c -eq 0 ]]
 	then
-		echo "HH";
-		((HH++));
-	elif [[ $coin1 -eq 0 && $coin2 -eq 1 ]]
+		echo "HHH";
+		((HHH++));
+	elif [[ $a -eq 0 && $b -eq 0 && $c -eq 1 ]]
 	then
-		echo "HT";
-		((HT++));
-	elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+		echo "HHT";
+		((HHT++));
+	elif [[ $a -eq 0 && $b -eq 1 && $c -eq 0 ]]
 	then
-		echo "TH";
-		((TH++));
-	elif [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
+		echo "HTH";
+		((HTH++));
+	elif [[ $a -eq 0 && $b -eq 1 && $c -eq 1 ]]
 	then
-		echo "TT";
-		((TT++));
+		echo "HTT";
+		((HTT++));
 	fi
 done
 
-echo "HH wins : $HH times";
-echo "HT wins : $HT times";
-echo "TH wins : $TH times";
-echo "TT wins : $TT times";
+while [[ $THH -ne 5 && $THT -ne 5 && $TTH -ne 5 && $TTT -ne 5 ]]
+do
+        coin1=$((RANDOM%2));
+        a=$coin1;
+        coin2=$((RANDOM%2));
+        b=$coin2;
+        coin3=$((RANDOM%2));
+        c=$coin3;
+        if [[ $a -eq 1 && $b -eq 0 && $c -eq 0 ]]
+        then
+                echo "THH";
+                ((THH++));
+        elif [[ $a -eq 1 && $b -eq 0 && $c -eq 0 ]]
+        then
+                echo "THT";
+                ((THT++));
+        elif [[ $a -eq 1 && $b -eq 1 && $c -eq 0 ]]
+        then
+                echo "TTH";
+                ((TTH++));
+        elif [[ $a -eq 1 && $b -eq 1 && $c -eq 1 ]]
+        then
+                echo "TTT";
+                ((TTT++));
+        fi
+done
+
+echo "HHH wins : $HHH times";
+echo "HHT wins : $HHT times";
+echo "HTH wins : $HTH times";
+echo "HTT wins : $HTT times";
+echo "THH wins : $THH times";
+echo "THT wins : $THT times";
+echo "TTH wins : $TTH times";
+echo "TTT wins : $TTT times";
 
 declare -a counter;
 
-counter[0]=HH;
-counter[1]=HT;
-counter[2]=TH;
-counter[3]=TT;
+counter[0]=HHH;
+counter[1]=HHT;
+counter[2]=HTH;
+counter[3]=HTT;
+counter[4]=THH;
+counter[5]=THT;
+counter[6]=TTH;
+counter[7]=TTT;
 
 echo ${counter[@]}
 
-#percentage of singlet combination
-total_count=$(($HH + $HT + $TH + $TT ));
-echo "total no of wins : $total_count";
-
-percentage_of_HH=`echo $HH $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of HH wins : $percentage_of_HH";
-
-percentage_of_HT=`echo $HT $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of HT wins : $percentage_of_HT";
-
-percentage_of_TH=`echo $TH $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of TH wins : $percentage_of_TH";
-
-percentage_of_TT=`echo $TT $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of TT wins : $percentage_of_TT";
