2b063884ed2930ace375e14931a836a261605e97
Komal <divyavairagade@gmail.com>
2022-04-03 15:05:13 +0530

    doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,37 +1,59 @@
 #!/bin/bash
 
-headcount=0;
-tailcount=0;
+HH=0;
+HT=0;
+TH=0;
+TT=0;
 
-while [[ headcount -ne 21 && tailcount -ne 21 ]]
+while [[ $HH -ne 5 && $HT -ne 5 && $TH -ne 5 && $TT -ne 5 ]]
 do
-	coin=$((RANDOM%2))
-	if [[ $coin -eq 0 ]]
+	coin1=$((RANDOM%2));
+	coin2=$((RANDOM%2));
+	if [[ $coin1 -eq 0 && $coin2 -eq 0 ]]
 	then
-		echo "HEAD";
-		((headcount++));
-	else
-		echo "TAIL";
-		((tailcount++));
+		echo "HH";
+		((HH++));
+	elif [[ $coin1 -eq 0 && $coin2 -eq 1 ]]
+	then
+		echo "HT";
+		((HT++));
+	elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+	then
+		echo "TH";
+		((TH++));
+	elif [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
+	then
+		echo "TT";
+		((TT++));
 	fi
 done
 
-echo "head wins : $headcount times";
-echo "tail wins : $tailcount times";
+echo "HH wins : $HH times";
+echo "HT wins : $HT times";
+echo "TH wins : $TH times";
+echo "TT wins : $TT times";
 
 declare -a counter;
 
-counter[0]=head;
-counter[1]=tail;
+counter[0]=HH;
+counter[1]=HT;
+counter[2]=TH;
+counter[3]=TT;
 
 echo ${counter[@]}
 
 #percentage of singlet combination
-total_count=$(( $headcount + $tailcount ));
+total_count=$(($HH + $HT + $TH + $TT ));
 echo "total no of wins : $total_count";
 
-percentage_of_head=`echo $headcount $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of head wins : $percentage_of_head";
+percentage_of_HH=`echo $HH $total_count 100|awk '{ print ($1/$2)*$3 }'`;
+echo "percentage of HH wins : $percentage_of_HH";
 
-percentage_of_tail=`echo $tailcount $total_count 100|awk '{ print ($1/$2)*$3 }'`;
-echo "percentage of tail wins : $percentage_of_tail";
+percentage_of_HT=`echo $HT $total_count 100|awk '{ print ($1/$2)*$3 }'`;
+echo "percentage of HT wins : $percentage_of_HT";
+
+percentage_of_TH=`echo $TH $total_count 100|awk '{ print ($1/$2)*$3 }'`;
+echo "percentage of TH wins : $percentage_of_TH";
+
+percentage_of_TT=`echo $TT $total_count 100|awk '{ print ($1/$2)*$3 }'`;
+echo "percentage of TT wins : $percentage_of_TT";
